f92f3cd2fb72f75e77fa10b078d57a977dcaabb5
Carrington Perry <carringtonperry@Carringtons-MacBook-Pro.local>
2018-08-30 09:23:31 -0400

    init setup

--- src/component/App.js
+++ src/component/App.js
@@ -1,16 +1,51 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 
 
 
 class App extends Component {
 
+
+
   render(){
+    Infohpapi()
+    Infopotterapi()
+    console.log(harryPotterSpells.all)
     return(
+      <div>
       <h1>hello</h1>
+
+      </div>
     )
   }
 
+}
+
+const harryPotterSpells = require('harry-potter-spells')
+
+function Infohpapi () {
+
+  const  request =
+   axios.get(`http://hp-api.herokuapp.com/api/characters`);
+
+    console.log("request", request);
+
+  return{ payload: request}
+}
+
+function Infopotterapi () {
+
+  const key= "$2a$10$O4hd7lWpr2K95sV68FMW/uWbh54LNxWort6gLLPiPJ47bNY2F9t82";
+  const url = "https://www.potterapi.com/v1/";
+  const search = "houses"
+  const request2= axios.get(`${url}${search}?key=${key}`);
+
+
+    //`${url}/characters?key=${key}`);
+
+    console.log("request2:", request2);
+
+  return{ payload: request2}
 }
 
 
